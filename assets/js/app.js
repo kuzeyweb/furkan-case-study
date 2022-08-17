ff09d49753836e53c62361f39c9ccfb879e31817
kuzeyweb <sfurkanpehlivan@gmail.com>
2022-08-17 22:18:16 +0300

    fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,10 +8,27 @@ const prev = document.querySelector('.icon-chevron-left');
 const prevButton = document.querySelector('.arrow-left');
 const nextButton = document.querySelector('.arrow-right');
 
-  
-  var mob_view = window.matchMedia("(max-width: 768px)")
+
+var mob_view = window.matchMedia("(max-width: 768px)")
 
 let progress = 0;
+
+function isElementInViewport() {
+    var bounding = document.querySelector("div.carousel-container > div > div:last-child").getBoundingClientRect();
+    if (bounding.top >= 0 && bounding.left >= 0 && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)) {
+        return true
+    } else {
+        return false
+    }
+}
+function isElementInViewportMobile() {
+    var bounding = document.querySelector("div.carousel-container > div > div:nth-last-child(2)").getBoundingClientRect();
+    if (bounding.top >= 0 && bounding.left >= 0 && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)) {
+        return true
+    } else {
+        return false
+    }
+}
 
 next.addEventListener('click', e => {
     if (progress < itemCount - 1) {
@@ -21,19 +38,18 @@ next.addEventListener('click', e => {
     if (progress > 0) {
         prevButton.classList.remove("hidden");
     }
-    if(progress == 1){
+    if (progress == 1) {
         carousel.style.setProperty("--slide", '-17.5%');
     }
-    if (progress == 1){
+    if (progress == 1) {
         carousel.style.setProperty("--slide", '-16.5%');
     }
-    if (mob_view.matches === true & progress === Math.ceil(itemCount / 2) + 1){
-        nextButton.classList.add("hidden");
-    }
-    else if(mob_view.matches === false & progress === itemCount - 2 ) {
-        nextButton.classList.add("hidden");
-    }
+    if(mob_view.matches === true && isElementInViewportMobile() === true){
+            nextButton.classList.add("hidden");
+    }else if(mob_view.matches === false && isElementInViewport() === true){
+            nextButton.classList.add("hidden");
 
+    }
 })
 prev.addEventListener('click', e => {
     if (progress > 0) {
